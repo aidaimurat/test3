6bc4cfdd3cd21fc1245938c23f06debfdd91fbe4
aidaimurat <64561162+aidaimurat@users.noreply.github.com>
2021-05-09 17:24:28 +0300

    Add files via upload

--- test3.py
+++ test3.py
@@ -1,26 +1,2 @@
 #1
 
-class BaseClass:
-  Body of base class
-class DerivedClass(BaseClass):
-  Body of derived class
-
-class Person( object ):    
-  
-        # __init__ is known as the constructor         
-        def __init__(self, name, idnumber):   
-                self.name = name
-                self.idnumber = idnumber
-        def display(self):
-                print(self.name)
-                print(self.idnumber)
-  
-# child class
-class Employee( Person ):           
-        def __init__(self, name, idnumber, salary, post):
-                self.salary = salary
-                self.post = post
-                Person.__init__(self, name, idnumber)
-
-#2
-
