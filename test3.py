d96ce1c5ae92bb753efe6e4445c0ab95c791258e
aidaimurat <64561162+aidaimurat@users.noreply.github.com>
2021-05-09 17:30:11 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,2 +1,14 @@
-#1
+#6
+class Encapsulation:
+    __name = None
+ 
+    def __init__(self, name):
+        self.__name = name
+ 
+    def get_name(self):
+        return self.__name
+ 
+ 
+a = Encapsulation('Aidai')
+print(a.get_name())
 
