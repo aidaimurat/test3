d39b5b30badc8628d40ac98a1af45e6c600bdbcb
aidaimurat <64561162+aidaimurat@users.noreply.github.com>
2021-05-09 19:55:41 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -69,3 +69,61 @@ class Renault(Car):
             
             
 #9
+class MarsRoverComp():
+    def __init__(self, name, distance, maker):
+        self.rocket = Rocket(name, distance) # instantiating the base
+
+        self.maker = maker
+
+    def get_maker(self):
+        return "%s Launched by %s" % (self.rocket.name, self.maker)
+
+
+if __name__ == "__main__":
+    z = MarsRover("mars_rover2", "till Mars", "ISRO")
+    print(z.launch())
+    print(z.get_maker())
+
+#10
+
+my_list = [14, 27, 230, 13]
+ 
+my_iter = iter(my_list)
+ 
+print(next(my_iter))
+print(next(my_iter))
+ 
+print(my_iter.__next__())
+print(my_iter.__next__())
+ 
+next(my_iter)
+
+#11
+
+[i for in range(10) if not i%3] == list(i for i in range(10) if not i%3)
+
+#12
+class Message(object):
+    def message(self):
+        return 'Hello World'
+AnotherMessage = Message
+print AnotherMessage().message()
+
+#13
+class Mixin1(object):
+    def test(self):
+        print "Mixin1"
+
+class Mixin2(object):
+    def test(self):
+        print "Mixin2"
+
+class MyClass(BaseClass, Mixin1, Mixin2):
+    pass
+#14
+class Vehicle:
+    """docstring"""
+ 
+    def __init__(self):
+        """Constructor"""
+        pass
